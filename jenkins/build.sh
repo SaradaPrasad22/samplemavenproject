f5287006f9c2f5126a282730ec38fa1ebd3a09d2
SaradaPrasad22 <58641741+SaradaPrasad22@users.noreply.github.com>
2022-04-02 15:37:57 -0400

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,5 +3,10 @@ echo "*****************************************"
 echo "**************Building Jar***************"
 echo "*****************************************"
 docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn -B -DiskipTests clean package
-cp samplemavenproject/target/*.jar /jenkins/
+#copy the created jar to build location
+cp -f samplemavenproject/target/*.jar samplemavenproject/jenkins/
+echo "******************************************"
+echo "************Building docker image*********"
+echo "******************************************"
+cd jenkins/ && docker build -f dockerfile -t myapp
 
